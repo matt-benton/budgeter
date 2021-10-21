9e4545b8d58eb82c8d19a07dc5d143d3e60e28be
matt-benton <mattbenton@hey.com>
2021-10-21 11:07:55 -0500

    Add dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Expense;
 
@@ -9,10 +10,12 @@ class DashboardController extends Controller
 {
     public function showDashboard()
     {
-        $expenses = Expense::with('category')->get();
+        $expenses = Expense::orderBy('date')->with('category')->get();
+        $categories = Category::orderBy('name')->get();
 
-        return inertia('Dashboard',
-            ['expenses' => $expenses]
-        );
+        return inertia('Dashboard', [
+            'expenses' => $expenses,
+            'categories' => $categories,
+        ]);
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,51 @@
 <template>
     <Layout>
-        <table class="table">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Category</th>
+                    <th class="text-right">Percent</th>
+                    <th class="text-right">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="cat in displayedCategories">
+                    <td>{{ cat.name }}</td>
+                    <td class="text-right">{{ getCategoryPercent(cat.id) }}%</td>
+                    <td class="text-right">{{ formatAmount(getCategoryTotal(cat.id)) }}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td class="text-right">
+                        <b>{{ formatAmount(allExpensesTotal) }}</b>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table class="table table-striped">
             <thead>
                 <tr>
                     <th>Date</th>
                     <th>Expense</th>
-                    <th>Amount</th>
                     <th>Category</th>
+                    <th class="text-right">Amount</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="exp in expenses">
-                    <td>{{ exp.date }}</td>
+                    <td>{{ formatDate(exp.date) }}</td>
                     <td>{{ exp.name }}</td>
-                    <td>{{ exp.amount }}</td>
                     <td>{{ exp.category.name }}</td>
+                    <td class="text-right">{{ formatAmount(exp.amount) }}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-right">
+                        <b>{{ formatAmount(allExpensesTotal) }}</b>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -23,11 +54,39 @@
 
 <script>
 import Layout from '../Shared/Layout.vue'
+import { format } from 'date-fns'
 
 export default {
+    computed: {
+        allExpensesTotal() {
+            return this.sumExpenses(this.expenses)
+        },
+        displayedCategories() {
+            return this.categories.filter(cat => this.getCategoryTotal(cat.id) > 0)
+        },
+    },
+    methods: {
+        getCategoryTotal(categoryId) {
+            return this.sumExpenses(this.expenses.filter(exp => exp.category_id === categoryId))
+        },
+        getCategoryPercent(categoryId) {
+            let categoryTotal = this.getCategoryTotal(categoryId)
+
+            return Math.round((categoryTotal / this.allExpensesTotal) * 100)
+        },
+        sumExpenses(expenses) {
+            return expenses.reduce((acc, currentExpense) => acc + currentExpense.amount, 0)
+        },
+        formatAmount(amount) {
+            return amount / 100
+        },
+        formatDate(date) {
+            return format(new Date(date), 'MMM do')
+        },
+    },
     components: {
         Layout,
     },
-    props: ['expenses'],
+    props: ['expenses', 'categories'],
 }
 </script>
