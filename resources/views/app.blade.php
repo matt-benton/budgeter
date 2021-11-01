0877473f3e7b6764fc50e6ff7a955634bbbe65b5
matt-benton <mattbenton@hey.com>
2021-11-01 13:48:57 -0500

    Add app name to title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <title>{{ env('APP_NAME') }}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
