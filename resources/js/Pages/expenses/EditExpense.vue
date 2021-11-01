a23fb6698046acbcee56b8e4fd1c02261a2b8a23
matt-benton <mattbenton@hey.com>
2021-11-01 15:43:59 -0500

    Add breadcrumb to expense editing page

--- resources/js/Pages/expenses/EditExpense.vue
+++ resources/js/Pages/expenses/EditExpense.vue
@@ -1,53 +1,69 @@
 <template>
     <Layout>
-        <h2>Edit Expense</h2>
-        <div class="card">
-            <form @submit.prevent="editForm.put(`/expenses/${expense.id}`)">
-                <div class="form-group">
-                    <label for="amount">Amount</label>
-                    <input type="text" id="amount" v-model="editForm.amount" class="form-control" />
-                </div>
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" id="name" v-model="editForm.name" class="form-control" />
-                </div>
-                <div class="form-group">
-                    <label for="date">Date</label>
-                    <input type="date" id="date" v-model="editForm.date" class="form-control" />
-                </div>
-                <div class="form-group">
-                    <label for="category">Category</label>
-                    <select class="form-control" id="category" v-model="editForm.category_id">
-                        <option v-for="category in categories" :value="category.id">
-                            {{ category.name }}
-                        </option>
-                    </select>
-                </div>
-                <input type="submit" class="btn btn-primary" value="Save" />
-            </form>
-        </div>
-        <div class="card">
-            <h3>Delete Expense</h3>
-            <form @submit.prevent="deleteExpense">
-                <div class="form-group">
-                    <label for="delete" id="delete">Type 'delete' to delete expense</label>
-                    <input
-                        type="text"
-                        v-model="deleteConfirm"
-                        class="form-control"
-                        id="delete"
-                        autocomplete="off"
-                    />
-                </div>
-                <input type="submit" class="btn btn-default" value="Delete Expense" />
-            </form>
+        <div class="content">
+            <nav>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><Link href="/">Home</Link></li>
+                    <li class="breadcrumb-item">
+                        <Link :href="`/expenses/${expense.id}`">{{ expense.name }}</Link>
+                    </li>
+                    <li class="breadcrumb-item active">Edit</li>
+                </ul>
+            </nav>
+            <h2>Edit Expense</h2>
+            <div class="card">
+                <form @submit.prevent="editForm.put(`/expenses/${expense.id}`)">
+                    <div class="form-group">
+                        <label for="amount">Amount</label>
+                        <input
+                            type="text"
+                            id="amount"
+                            v-model="editForm.amount"
+                            class="form-control"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" v-model="editForm.name" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="date">Date</label>
+                        <input type="date" id="date" v-model="editForm.date" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Category</label>
+                        <select class="form-control" id="category" v-model="editForm.category_id">
+                            <option v-for="category in categories" :value="category.id">
+                                {{ category.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Save" />
+                </form>
+            </div>
+            <div class="card">
+                <h3>Delete Expense</h3>
+                <form @submit.prevent="deleteExpense">
+                    <div class="form-group">
+                        <label for="delete" id="delete">Type 'delete' to delete expense</label>
+                        <input
+                            type="text"
+                            v-model="deleteConfirm"
+                            class="form-control"
+                            id="delete"
+                            autocomplete="off"
+                        />
+                    </div>
+                    <input type="submit" class="btn btn-default" value="Delete Expense" />
+                </form>
+            </div>
         </div>
     </Layout>
 </template>
 
 <script>
 import Layout from '../../Shared/Layout.vue'
-import { useForm } from '@inertiajs/inertia-vue3'
+import { useForm, Link } from '@inertiajs/inertia-vue3'
 
 export default {
     data() {
@@ -75,6 +91,7 @@ export default {
     },
     components: {
         Layout,
+        Link,
     },
     props: ['expense', 'categories'],
 }
